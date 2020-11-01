a00b71e1c3ffd509c70f3f5d7234540c488e5db9
Allen <howlmoon123@gmail.com>
2020-11-01 08:31:14 -0600

    Created a bunch of files
    Started going through course for this portion will alter later for my puposes.

---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,34 @@
+﻿public enum GridBoolProperty
+{
+    diggable,
+    canDropItem,
+    canPlaceFurniture,
+    isPath,
+    isNPCObstacle
+}
+
+public enum InventoryLocation
+{
+    player,
+    chest,
+    count
+}
+
+public enum SceneName
+{
+    Scene1_Farm,
+    Scene2_Field,
+    Scene3_Cabin
+}
+
+public enum Season
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter,
+    none,
+    count
+}
+
+

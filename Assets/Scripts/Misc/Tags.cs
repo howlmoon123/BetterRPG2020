9ad1b7f49baf17e7a97488e46b823bfe70a607ea
Allen <howlmoon123@gmail.com>
2020-11-03 17:29:27 -0600

    Revert "Revert "Inventory Basics""
    This reverts commit f3a871d6fdb05b578e0b1500cda88cb78a6e98da.

---
+++ Assets/Scripts/Misc/Tags.cs
@@ -0,0 +1,12 @@
+﻿
+
+public static class Tags 
+{
+    public const string BoundsConfiner = "BoundConfiner";
+    public const string CropsParentTransform = "CropsParentTransform";
+    public const string GroundDecoration1 = "GroundDecoration1";
+    public const string GroundDecoration2 = "GroundDecoration2";
+    public const string Player = "Player";
+
+    public const string ItemsParentTransform = "ItemsParentTransform";
+}

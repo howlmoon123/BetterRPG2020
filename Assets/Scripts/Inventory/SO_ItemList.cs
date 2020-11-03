7c7355041ecd30f351f4ab2caacdd45c76a9737e
Allen <howlmoon123@gmail.com>
2020-11-03 17:51:34 -0600

    InventDoneory Basics
    Finished InventoryBasics

--- Assets/Scripts/Inventory/SO_ItemList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="SO_ItemList", menuName = "Scriptable Objects/Item List")]
-public class SO_ItemList : ScriptableObject
-{
-    [SerializeField]
-    public List<ItemDetails> itemDetails;
-}
--- Assets/Tilemap/Custom Brush Scripts/Coordinate Brush/Editor/CoordinateBrush.cs
+++ Assets/Tilemap/Custom Brush Scripts/Coordinate Brush/Editor/CoordinateBrush.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor.Tilemaps;
 using UnityEngine;
-using UnityEditor.Tilemaps;
 
 namespace UnityEditor
 {
